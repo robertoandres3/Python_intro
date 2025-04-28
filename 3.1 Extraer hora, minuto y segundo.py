2650982271d86e91d4410a9a8892514bef87bb74
robertoandres3 <157542797+robertoandres3@users.noreply.github.com>
2025-04-28 00:59:48 -0500

    Update 3.1 Extraer hora, minuto y segundo.py

---
+++ 3.1 Extraer hora, minuto y segundo.py
@@ -0,0 +1,15 @@
+  #  3 Extraer hora, minuto y segundo de segundos totales
+
+                numersegundo= int(input("Escribe los segundos en numeros sin puntos ni comas : "))
+
+                # 1 hora = 60 minuutos y 3600 segundos 
+                # 1 minut = 60 segund
+
+                horas = numersegundo // 3600
+                segundos_restantes = numersegundo % 3600
+                minutos = segundos_restantes // 60
+
+                # Segundos que sobran después de sacar los minutos
+                segundos = segundos_restantes % 60
+
+                print (f"{horas} hora(s), {minutos} minuto(s), {segundos} segundo(s).")
