1cd3571fcf333fb688e5c1e2e84109062918d595
robertoandres3 <157542797+robertoandres3@users.noreply.github.com>
2025-04-28 01:06:28 -0500

    Update 4.1 Fecha formateada.py

---
+++ 4.1 Fecha formateada.py
@@ -0,0 +1,8 @@
+# 4 Fecha formateada
+
+dia = input("Introduce el día: ")
+mes = input("Introduce el mes: ")
+año = input("Introduce el año: ")
+
+print(f"{dia}/{mes}/{año}")
+print(f"{año}-{mes}-{dia}")
